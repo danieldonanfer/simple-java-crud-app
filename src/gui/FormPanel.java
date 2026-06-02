7bf9a88a193ba812c6579e7ec2863aba8c97a261
Daniel Fernando <153383270+danieldonanfer@users.noreply.github.com>
2026-06-02 19:27:31 -0300

    refactor: reformula FormPanel para layout de RH com Data de Nascimento

--- src/gui/FormPanel.java
+++ src/gui/FormPanel.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -6,18 +7,14 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.net.URL;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.DefaultListModel;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -26,99 +23,93 @@ import javax.swing.border.Border;
 public class FormPanel extends JPanel {
 
 	private JLabel nameLabel;
+	private JLabel cpfLabel;
+	private JLabel nascimentoLabel;
 	private JLabel occupationLabel;
 	private JLabel emailLabel;
 	private JLabel telefoneLabel;
+	private JLabel contratoLabel;
+	private JLabel ativoLabel;
+	private JLabel generoLabel;
+
 	private JTextField nameField;
+	private JTextField cpfField;
+	private JTextField nascimentoField;
 	private JTextField occupationField;
 	private JTextField emailField;
 	private JTextField telefoneField;
-	private JButton okBtn;
-	private FormListener formListener;
-	private JList ageList;
-	private JComboBox empCombo;
+
+	private JComboBox contratoCombo;
 	private JCheckBox ativoCheck;
-	private JTextField taxField;
-	private JLabel taxLabel;
 
 	private JRadioButton maleRadio;
 	private JRadioButton femaleRadio;
 	private ButtonGroup genderGroup;
 
+	private JButton okBtn;
+	private FormListener formListener;
+
 	public FormPanel() {
 		Dimension dim = getPreferredSize();
-		dim.width = 290;
+		dim.width = 300;
 		setPreferredSize(dim);
 
-		nameLabel      = new JLabel("Nome: ");
-		nameLabel.setIcon(createIcon("/images/gear.png"));
-		occupationLabel = new JLabel("Ocupacao: ");
-		occupationLabel.setIcon(createIcon("/images/price.png"));
-		emailLabel     = new JLabel("E-mail: ");
-		telefoneLabel  = new JLabel("Telefone: ");
+		nameLabel       = new JLabel("Nome:");
+		cpfLabel        = new JLabel("CPF:");
+		nascimentoLabel = new JLabel("Nascimento:");
+		occupationLabel = new JLabel("Cargo:");
+		emailLabel      = new JLabel("E-mail:");
+		telefoneLabel   = new JLabel("Telefone:");
+		contratoLabel   = new JLabel("Contrato:");
+		ativoLabel      = new JLabel("Ativo:");
+		generoLabel     = new JLabel("Genero:");
 
-		nameField      = new JTextField(10);
-		occupationField = new JTextField(10);
-		emailField     = new JTextField(10);
-		telefoneField  = new JTextField(10);
+		nameField       = new JTextField(12);
+		cpfField        = new JTextField(12);
+		nascimentoField = new JTextField(12);
+		nascimentoField.setToolTipText("DD/MM/AAAA");
+		occupationField = new JTextField(12);
+		emailField      = new JTextField(12);
+		telefoneField   = new JTextField(12);
 
-		ageList   = new JList();
-		empCombo  = new JComboBox();
+		DefaultComboBoxModel contratoModel = new DefaultComboBoxModel();
+		contratoModel.addElement("CLT");
+		contratoModel.addElement("PJ");
+		contratoModel.addElement("Estagiario");
+		contratoModel.addElement("Terceirizado");
+		contratoCombo = new JComboBox(contratoModel);
+		contratoCombo.setSelectedIndex(0);
+
 		ativoCheck = new JCheckBox();
-		taxField  = new JTextField(10);
-		taxLabel  = new JLabel("CPF: ");
-		okBtn     = new JButton("OK");
-		okBtn.setIcon(createIcon("/images/1400445397_accept.png"));
-
-		okBtn.setMnemonic(KeyEvent.VK_O);
-		nameLabel.setDisplayedMnemonic(KeyEvent.VK_N);
-		nameLabel.setLabelFor(nameField);
+		ativoCheck.setSelected(true);
 
 		maleRadio   = new JRadioButton("Masculino");
 		femaleRadio = new JRadioButton("Feminino");
 		maleRadio.setActionCommand("MASCULINO");
 		femaleRadio.setActionCommand("FEMININO");
+		maleRadio.setSelected(true);
 
 		genderGroup = new ButtonGroup();
-		maleRadio.setSelected(true);
 		genderGroup.add(maleRadio);
 		genderGroup.add(femaleRadio);
 
-		// Lista de faixas etarias
-		DefaultListModel ageModel = new DefaultListModel();
-		ageModel.addElement(new AgeCategory(0, "Crianca"));
-		ageModel.addElement(new AgeCategory(1, "Adolescente"));
-		ageModel.addElement(new AgeCategory(2, "Adulto"));
-		ageModel.addElement(new AgeCategory(3, "Meia-Idade"));
-		ageModel.addElement(new AgeCategory(4, "Idoso"));
-		ageList.setModel(ageModel);
-		ageList.setPreferredSize(new Dimension(120, 100));
-		ageList.setBorder(BorderFactory.createEtchedBorder());
-		ageList.setSelectedIndex(2);
-
-		// Combo de emprego
-		DefaultComboBoxModel empModel = new DefaultComboBoxModel();
-		empModel.addElement("Empregado");
-		empModel.addElement("Desempregado");
-		empModel.addElement("Autonomo");
-		empCombo.setModel(empModel);
-		empCombo.setSelectedIndex(0);
-		empCombo.setEditable(true);
+		okBtn = new JButton("Cadastrar");
+		okBtn.setMnemonic(KeyEvent.VK_C);
 
 		okBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String name       = nameField.getText();
-				String occupation = occupationField.getText();
-				AgeCategory ageCat = (AgeCategory) ageList.getSelectedValue();
-				String empCat     = (String) empCombo.getSelectedItem();
-				String taxId      = taxField.getText();
-				boolean ativo     = ativoCheck.isSelected();
-				String gender     = genderGroup.getSelection().getActionCommand();
-				String email      = emailField.getText();
-				String telefone   = telefoneField.getText();
+				String name           = nameField.getText();
+				String cpf            = cpfField.getText();
+				String nascimento     = nascimentoField.getText();
+				String occupation     = occupationField.getText();
+				String email          = emailField.getText();
+				String telefone       = telefoneField.getText();
+				String contrato       = (String) contratoCombo.getSelectedItem();
+				boolean ativo         = ativoCheck.isSelected();
+				String gender         = genderGroup.getSelection().getActionCommand();
 
 				FormEvent ev = new FormEvent(this, name, occupation,
-						ageCat.getId(), empCat, ativo, taxId, gender, email, telefone);
+						nascimento, contrato, ativo, cpf, gender, email, telefone);
 
 				if (formListener != null) {
 					formListener.formEventOccurred(ev);
@@ -126,117 +117,96 @@ public class FormPanel extends JPanel {
 			}
 		});
 
-		Border innerBorder = BorderFactory.createTitledBorder("Adicionar Pessoa");
+		Border innerBorder = BorderFactory.createTitledBorder("Dados do Colaborador");
 		Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
 		layoutComponents();
 	}
 
-	private ImageIcon createIcon(String path) {
-		URL url = getClass().getResource(path);
-		if (url == null) System.err.println("Unable to load image: " + path);
-		return new ImageIcon(url);
-	}
-
 	public void layoutComponents() {
 		setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
 
+		Insets labelInsets = new Insets(4, 0, 4, 6);
+		Insets fieldInsets = new Insets(4, 0, 4, 0);
+
+		int row = 0;
+
 		// Nome
-		gc.gridy = 0; gc.weightx = 1; gc.weighty = 0.1;
-		gc.gridx = 0; gc.fill = GridBagConstraints.NONE;
-		gc.anchor = GridBagConstraints.LINE_END; gc.insets = new Insets(0,0,0,5);
+		gc.gridy = row; gc.weightx = 1; gc.weighty = 0.1;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
 		add(nameLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = new Insets(0,0,0,0);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
 		add(nameField, gc);
 
-		// Ocupacao
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = new Insets(0,0,0,5);
-		add(occupationLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = new Insets(0,0,0,0);
-		add(occupationField, gc);
+		// CPF
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
+		add(cpfLabel, gc);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(cpfField, gc);
 
-		// E-mail
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = new Insets(0,0,0,5);
+		// Nascimento
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
+		add(nascimentoLabel, gc);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(nascimentoField, gc);
+
+		// Genero
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
+		add(generoLabel, gc);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(maleRadio, gc);
+
+		gc.gridy = ++row;
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(femaleRadio, gc);
+
+		// Email
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
 		add(emailLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = new Insets(0,0,0,0);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
 		add(emailField, gc);
 
 		// Telefone
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = new Insets(0,0,0,5);
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
 		add(telefoneLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = new Insets(0,0,0,0);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
 		add(telefoneField, gc);
 
-		// Faixa Etaria
-		gc.gridy++; gc.weighty = 0.2;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.FIRST_LINE_END; gc.insets = new Insets(5,0,0,5);
-		JLabel faixaLabel = new JLabel("Faixa Etaria: ");
-		faixaLabel.setIcon(createIcon("/images/car.png"));
-		add(faixaLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(5,0,0,0);
-		add(ageList, gc);
+		// Cargo
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
+		add(occupationLabel, gc);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(occupationField, gc);
 
-		// Emprego
-		gc.gridy++; gc.weighty = 0.2;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.FIRST_LINE_END; gc.insets = new Insets(0,0,0,5);
-		JLabel empLabel = new JLabel("Emprego: ");
-		empLabel.setIcon(createIcon("/images/ware.png"));
-		add(empLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(0,0,0,0);
-		add(empCombo, gc);
+		// Tipo de Contrato
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
+		add(contratoLabel, gc);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
+		add(contratoCombo, gc);
 
 		// Ativo
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.FIRST_LINE_END; gc.insets = new Insets(0,0,0,5);
-		JLabel ativoLabel = new JLabel("Ativo: ");
-		ativoLabel.setIcon(createIcon("/images/import1.png"));
+		gc.gridy = ++row;
+		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = labelInsets;
 		add(ativoLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(0,0,0,0);
+		gc.gridx = 1; gc.anchor = GridBagConstraints.LINE_START; gc.insets = fieldInsets;
 		add(ativoCheck, gc);
 
-		// CPF (sempre visivel)
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.FIRST_LINE_END; gc.insets = new Insets(0,0,0,5);
-		add(taxLabel, gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(0,0,0,0);
-		add(taxField, gc);
-
-		// Genero
-		gc.gridy++; gc.weighty = 0.05;
-		gc.gridx = 0; gc.anchor = GridBagConstraints.LINE_END; gc.insets = new Insets(0,0,0,5);
-		add(new JLabel("Genero: "), gc);
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(0,0,0,0);
-		add(maleRadio, gc);
-
-		gc.gridy++; gc.weighty = 0.1;
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(0,0,0,0);
-		add(femaleRadio, gc);
-
-		// OK
-		gc.gridy++; gc.weighty = 2.0;
-		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(5,0,0,0);
+		// Botao
+		gc.gridy = ++row; gc.weighty = 2.0;
+		gc.gridx = 1; gc.anchor = GridBagConstraints.FIRST_LINE_START; gc.insets = new Insets(8, 0, 0, 0);
 		add(okBtn, gc);
 	}
 
 	public void setFormListener(FormListener listener) {
 		this.formListener = listener;
 	}
-}
-
-class AgeCategory {
-	private int id;
-	private String text;
-
-	public AgeCategory(int id, String text) {
-		this.id = id;
-		this.text = text;
-	}
-
-	public String toString() { return text; }
-	public int getId() { return id; }
 }
