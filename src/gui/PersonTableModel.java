c7cac2fdc3f874819cf0d39e5493cd78b9744fe4
Daniel Fernando <153383270+danieldonanfer@users.noreply.github.com>
2026-06-02 19:27:57 -0300

    refactor: atualiza colunas da tabela para modelo de RH

--- src/gui/PersonTableModel.java
+++ src/gui/PersonTableModel.java
@@ -11,72 +11,80 @@ public class PersonTableModel extends AbstractTableModel {
 
 	private List<Person> db;
 
-	private String[] colNames = {"ID", "Nome", "Ocupacao", "Faixa Etaria", "Emprego", "Ativo", "CPF", "E-mail", "Telefone"};
-
-	private int colLength = colNames.length;
+	private String[] colNames = {
+		"ID", "Nome", "CPF", "Nascimento", "Genero",
+		"Cargo", "Contrato", "Ativo", "E-mail", "Telefone"
+	};
 
 	public PersonTableModel() {}
-
-	@Override
-	public String getColumnName(int column) {
-		return colNames[column];
-	}
 
 	public void setData(List<Person> db) {
 		this.db = db;
 	}
 
 	@Override
+	public String getColumnName(int col) {
+		return colNames[col];
+	}
+
+	@Override
+	public int getColumnCount() {
+		return colNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return db == null ? 0 : db.size();
+	}
+
+	@Override
 	public Class<?> getColumnClass(int col) {
 		switch (col) {
 			case 0: return Integer.class;
-			case 5: return Boolean.class;
-			case 4: return EmploymentCategory.class;
+			case 6: return EmploymentCategory.class;
+			case 7: return Boolean.class;
 			default: return String.class;
+		}
+	}
+
+	@Override
+	public boolean isCellEditable(int row, int col) {
+		// ID, Nascimento e Genero nao sao editaveis direto na tabela
+		switch (col) {
+			case 0: case 3: case 4: return false;
+			default: return true;
 		}
 	}
 
 	@Override
 	public void setValueAt(Object value, int row, int col) {
 		if (db == null) return;
-		Person person = db.get(row);
+		Person p = db.get(row);
 		switch (col) {
-			case 1: person.setName((String) value); break;
-			case 2: person.setOccupation((String) value); break;
-			case 4: person.setEmpCat((EmploymentCategory) value); break;
-			case 5: person.setAtivo((Boolean) value); break;
-			case 7: person.setEmail((String) value); break;
-			case 8: person.setTelefone((String) value); break;
+			case 1: p.setName((String) value);                   break;
+			case 2: p.setTaxId((String) value);                  break;
+			case 5: p.setOccupation((String) value);             break;
+			case 6: p.setEmpCat((EmploymentCategory) value);     break;
+			case 7: p.setAtivo((Boolean) value);                 break;
+			case 8: p.setEmail((String) value);                  break;
+			case 9: p.setTelefone((String) value);               break;
 		}
 	}
-
-	@Override
-	public boolean isCellEditable(int row, int col) {
-		switch (col) {
-			case 1: case 2: case 4: case 5: case 7: case 8: return true;
-			default: return false;
-		}
-	}
-
-	@Override
-	public int getColumnCount() { return colLength; }
-
-	@Override
-	public int getRowCount() { return db == null ? 0 : db.size(); }
 
 	@Override
 	public Object getValueAt(int row, int col) {
-		Person person = db.get(row);
+		Person p = db.get(row);
 		switch (col) {
-			case 0: return person.getId();
-			case 1: return person.getName();
-			case 2: return person.getOccupation();
-			case 3: return person.getAgeCategory();
-			case 4: return person.getEmpCat();
-			case 5: return person.isAtivo();
-			case 6: return person.getTaxId();
-			case 7: return person.getEmail();
-			case 8: return person.getTelefone();
+			case 0: return p.getId();
+			case 1: return p.getName();
+			case 2: return p.getTaxId();
+			case 3: return p.getDataNascimento();
+			case 4: return p.getGender();
+			case 5: return p.getOccupation();
+			case 6: return p.getEmpCat();
+			case 7: return p.isAtivo();
+			case 8: return p.getEmail();
+			case 9: return p.getTelefone();
 			default: return null;
 		}
 	}
